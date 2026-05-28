135c31a0218c3be0a0a8da1e7848a922836a3140
anshul9918 <25113cn008@glbitm.ac.in>
2026-05-28 12:16:47 +0530

    Added Question 4 of Day 1

---
+++ Day_1/Question_4.java
@@ -0,0 +1,28 @@
+// Count Number of Digits in given number 
+
+package Day_1;
+
+import java.util.Scanner;
+
+public class Question_4 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter the number to count digits in it : ");
+    int n = sc.nextInt();
+    int count = 0;
+    if (n==0){
+        count = 1;
+    }
+    else{
+        while(n!=0){
+            n=n/10;
+            count++;
+        }
+    }
+    System.out.println("Number of digits present in given number are : "+count);
+    sc.close();
+    }
+    
+
+
+}
