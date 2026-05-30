d24d4f9a1f9b3fdec67b49f3819cd4890f869f7a
anshul9918 <25113cn008@glbitm.ac.in>
2026-05-30 21:37:40 +0530

    Added Question 10 of Day 3

---
+++ Day_3/Question_10.java
@@ -0,0 +1,31 @@
+package Day_3;
+
+import java.util.Scanner;
+
+public class Question_10 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter starting number: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter ending number: ");
+        int end = sc.nextInt();
+
+        for (int num = start; num <= end; num++) {
+            int count = 0;
+
+            if (num < 2)
+                continue;
+
+            for (int i = 1; i <= num; i++) {
+                if (num % i == 0)
+                    count++;
+            }
+
+            if (count == 2)
+                System.out.print(num + " ");
+        }
+        sc.close();
+    }
+}
