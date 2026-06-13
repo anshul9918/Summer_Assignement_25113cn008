4b3bbab201d1b0373d61c9bf4b49cb8828eb10f2
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-13 20:57:26 +0530

    Added Question 64 of Day 16

---
+++ Day_16/Question_64.java
@@ -0,0 +1,42 @@
+package Day_16;
+import java.util.Scanner;
+
+public class Question_64 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the number of elements:");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int[] temp = new int[n];
+        int uniqueCount = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean isDuplicate = false;
+            
+            for (int j = 0; j < uniqueCount; j++) {
+                if (arr[i] == temp[j]) {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+            if (!isDuplicate) {
+                temp[uniqueCount] = arr[i];
+                uniqueCount++;
+            }
+        }
+
+        System.out.println("Array after removing duplicates:");
+        for (int i = 0; i < uniqueCount; i++) {
+            System.out.print(temp[i] + " ");
+        }
+
+        sc.close();
+    }
+}   
