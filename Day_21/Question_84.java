a7331ffc44d64d8a75954fe608e027eb510afd3c
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-18 21:23:33 +0530

    Added Question 84 of Day 21

---
+++ Day_21/Question_84.java
@@ -0,0 +1,19 @@
+// Uppercase the string
+package Day_21;
+
+import java.util.Scanner;
+
+public class Question_84 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String upper = str.toUpperCase();
+
+        System.out.println("Uppercase String = " + upper);
+
+        sc.close();
+    }
+}
