0f98a674091932d98f7f4d94bc587b7a1fc5ecee
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-18 21:21:18 +0530

    Added Question 82 of Day 21

---
+++ Day_21/Question_82.java
@@ -0,0 +1,23 @@
+//Reverse the string
+package Day_21;
+
+import java.util.Scanner;
+
+public class Question_82 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String rev = "";
+
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println("Reversed String = " + rev);
+
+        sc.close(); 
+    }
+}
