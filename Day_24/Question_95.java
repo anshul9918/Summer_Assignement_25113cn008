4ad0abaab01686e308bf3b63785de1d3f92c3410
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-21 20:53:33 +0530

    Added Question 95 of Day 24

---
+++ Day_24/Question_95.java
@@ -0,0 +1,28 @@
+// Find longest word in sentence :
+
+package Day_24;
+
+import java.util.Scanner;
+
+public class Question_95 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String str = sc.nextLine();
+
+        String[] words = str.split(" ");
+
+        String longest = words[0];
+
+        for (String word : words) {
+            if (word.length() > longest.length()) {
+                longest = word;
+            }
+        }
+
+        System.out.println("Longest Word = " + longest);
+
+        sc.close();
+    }
+}
