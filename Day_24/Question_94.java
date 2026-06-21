63b7415056dc3d79d30f62c4b8738ac83949d3da
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-21 20:52:36 +0530

    Added Question 94 of Day 24

---
+++ Day_24/Question_94.java
@@ -0,0 +1,31 @@
+//  Compress a string
+package Day_24;
+
+import java.util.Scanner;
+
+public class Question_94 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String str = sc.nextLine();
+
+        String result = "";
+        int count = 1;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            if (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+            } else {
+                result += str.charAt(i);
+                result += count;
+                count = 1;
+            }
+        }
+
+        System.out.println("Compressed String = " + result);
+
+        sc.close();
+    }
+}
