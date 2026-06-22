a0751aa74fd797197a9667610ac4d53bd65afb78
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-22 19:32:30 +0530

    Added Question 99 of Day 25

---
+++ Day_25/Question_99.java
@@ -0,0 +1,44 @@
+package Day_25;
+
+import java.util.Scanner;
+
+// Q99. Write a program to Sort names alphabetically.
+public class Question_99 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter number of names: ");
+        int n = sc.nextInt();
+        sc.nextLine();
+
+        String[] names = new String[n];
+
+        System.out.println("Enter names:");
+
+        for (int i = 0; i < n; i++) {
+            names[i] = sc.nextLine();
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                if (names[i].compareToIgnoreCase(names[j]) > 0) {
+
+                    String temp = names[i];
+                    names[i] = names[j];
+                    names[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Names in Alphabetical Order:");
+
+        for (String name : names) {
+            System.out.println(name);
+        }
+
+        sc.close();
+    }
+}
