850be906a5561718932feb1b3e930c8b124fffc8
anshul9918 <25113cn008@glbitm.ac.in>
2026-06-22 19:34:15 +0530

    Added Question 100 of Day 25

---
+++ Day_25/Question_100.java
@@ -0,0 +1,38 @@
+package Day_25;
+
+import java.util.Scanner;
+
+// Q100. Write a program to Sort words by length.
+public class Question_100 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a sentence: ");
+        String sentence = sc.nextLine();
+
+        String[] words = sentence.split(" ");
+
+        for (int i = 0; i < words.length - 1; i++) {
+
+            for (int j = i + 1; j < words.length; j++) {
+
+                if (words[i].length() > words[j].length()) {
+
+                    String temp = words[i];
+                    words[i] = words[j];
+                    words[j] = temp;
+                }
+            }
+        }
+
+        System.out.println("Words Sorted by Length:");
+
+        for (String word : words) {
+            System.out.print(word + " ");
+        }
+
+        sc.close();
+    }
+}
